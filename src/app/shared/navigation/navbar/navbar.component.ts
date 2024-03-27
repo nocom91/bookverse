06585f8b457a8019582007d3c1423706a6db07bf
Anastasiya Kostina <anastasiya.kostina@rubius.com>
2024-03-27 22:58:05 +0500

    Add an icon button into the sidebar

--- src/app/shared/navigation/navbar/navbar.component.ts
+++ src/app/shared/navigation/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'bv-navbar',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule],
+  imports: [MatSidenavModule, MatButtonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
